ece622ecf4d738ca19d5281d15a0a485a292b40a
ppaprikaa <sanzhar.maratov@proton.me>
2024-06-15 11:33:47 +0500

    configuration type and API

---
+++ conf.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef CONF_H
+#define CONF_H
+#include <stdint.h>
+
+typedef struct {
+	uint32_t screen_width;
+	uint32_t screen_height;
+	int frame_delay;
+} conf;
+
+int readconf(conf *c, char *filepath);
+
+#endif
